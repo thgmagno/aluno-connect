6c7371e6002e3dfeadab3c2caabd729392843ae5
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2023-12-31 13:37:09 -0300

    install shadcn-ui and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
-      <h1>Aluno Connect</h1>
+      <h1 className="text-4xl font-bold">Aluno Connect</h1>
     </main>
   )
 }
