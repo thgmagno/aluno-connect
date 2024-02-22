34fe956a0331133df3b7a1bc00cfff5dc5dfafea
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-02-22 15:08:05 -0300

    update footer.tsx

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 flex h-12 w-full items-center justify-center bg-neutral-950/45 text-sm text-primary-foreground">
-      <p className="text-center">
-        SENAI © 2024 Thiago Magno | (48) 99180-8906 | @thgmagno 🚀
-      </p>
+      <p className="text-center">SENAI © 2024 Thiago Magno</p>
     </footer>
   )
 }
